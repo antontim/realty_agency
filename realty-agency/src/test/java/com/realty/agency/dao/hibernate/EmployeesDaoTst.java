5bec449dce92d0aafa7aef859e4a80bc500fd82e
Anton <anton@anton-Inspiron-5520.(none)>
2013-05-08 03:42:34 +0300

    Implemented rates calculation

--- realty-agency/src/test/java/com/realty/agency/dao/hibernate/EmployeesDaoTst.java
+++ realty-agency/src/test/java/com/realty/agency/dao/hibernate/EmployeesDaoTst.java
@@ -1,28 +1,26 @@
 package com.realty.agency.dao.hibernate;
 
-import static org.junit.Assert.assertEquals;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import com.realty.agency.dao.IEmployeesDao;
-import com.realty.agency.domain.Employees;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "classpath:/applicationContext.xml" })
 public class EmployeesDaoTst {
+    private Logger logger = LoggerFactory.getLogger(EmployeesDaoTst.class);
     @Autowired
     private IEmployeesDao employeesDao;
     
     @Test
     public void test() {
-        List<Employees> res = this.employeesDao.findLastMonthTestResults();
+        this.employeesDao.calculateMonthEmpRates();
         
-        assertEquals(5,res.size());
+        logger.info("test");
     }
 }
