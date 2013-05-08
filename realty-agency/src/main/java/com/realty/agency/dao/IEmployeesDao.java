9051cc84f637597ee7d313fc47696d5aa12d2212
Anton <antoxatim.ua@gmail.com>
2013-05-08 03:42:34 +0300

    Implemented rates calculation

--- realty-agency/src/main/java/com/realty/agency/dao/IEmployeesDao.java
+++ realty-agency/src/main/java/com/realty/agency/dao/IEmployeesDao.java
@@ -1,12 +1,10 @@
 package com.realty.agency.dao;
 
-import java.util.List;
-
 import org.springframework.transaction.annotation.Transactional;
 
 import com.realty.agency.domain.Employees;
 
 @Transactional
 public interface IEmployeesDao extends IDao<Employees> {
-    List<Employees> findLastMonthTestResults();
+    void calculateMonthEmpRates();
 }
--- realty-agency/src/main/java/com/realty/agency/dao/hibernate/EmployeesDao.java
+++ realty-agency/src/main/java/com/realty/agency/dao/hibernate/EmployeesDao.java
@@ -1,18 +1,14 @@
 package com.realty.agency.dao.hibernate;
 
-import java.util.ArrayList;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
 
-import org.hibernate.Criteria;
-import org.hibernate.criterion.Order;
-import org.hibernate.criterion.Restrictions;
+import org.hibernate.Query;
 
 import com.realty.agency.dao.IEmployeesDao;
 import com.realty.agency.domain.Employees;
-import com.realty.agency.domain.Measures;
-import com.realty.agency.domain.TestResults;
 
 /**
  * Home object for domain model class Employees.
@@ -21,36 +17,54 @@ import com.realty.agency.domain.TestResults;
  */
 public class EmployeesDao extends HibernateDao<Employees> implements
         IEmployeesDao {
+    private final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
 
     public EmployeesDao() {
         super.setEntityName("com.realty.agency.domain.Employees");
     }
 
     @Override
-    public List<Employees> findLastMonthTestResults() {
+    public void calculateMonthEmpRates() {
         Calendar cal = Calendar.getInstance();
         cal.set(Calendar.DAY_OF_MONTH, 1);
+        cal.set(Calendar.MILLISECOND, 0);
+        cal.set(Calendar.SECOND, 0);
+        cal.set(Calendar.MINUTE, 0);
+        cal.set(Calendar.HOUR, 0);
         Date startDate = cal.getTime();
-        cal.add(Calendar.DAY_OF_MONTH, 40);
-        cal.set(Calendar.DAY_OF_MONTH, 0);
+        cal.add(Calendar.DAY_OF_MONTH, 33);
+        cal.set(Calendar.DAY_OF_MONTH, 1);
         Date endDate = cal.getTime();
-        List<Employees> res = new ArrayList<Employees>();
-        String query = "select emp.name, m.name, tr.result from Employees emp, Measures m"
-+ "left outer join emp.testResultses tr"
-+ "left outer join tr.tests t with t.measures.id = m.id"
-+ "order by emp.id, m.id";
+        String query = 
+"INSERT INTO agency.rates (measure_id,employee_id,created,value)"
++ " select m.id, emp.id, SYSDATE(), avg(COALESCE(tr.result,0)) +"
++     " COALESCE((select avg(ev_.mark) from agency.employee_evaluations ev_"
++         " inner join agency.questions q_ on q_.id = ev_.question_id"
++         " inner join agency.measures m_ on m_.id = q_.measure_id"
++         " inner join agency.employees emp_ on emp_.id = ev_.employee_id"
++         " where ev_.created between :startDate and :endDate"
++             " and emp_.id = emp.id and m_.id = m.id"
++         " group by m_.id),0)/10 as value"
++ " from agency.employees emp"
++ " left outer join agency.measures m on m.id <> -1"
++ " left outer join agency.tests t on m.id = t.measure_id"
++ " left outer join agency.test_results tr on emp.id = tr.employee_id and t.id = tr.test_id"
++ " where (tr.passed between :startDate and :endDate"
++     " and tr.passed = ("
++         " select max(tr1.passed)" 
++             " from agency.test_results tr1" 
++             " where tr1.employee_id = emp.id" 
++                 " and tr1.test_id = t.id)"
++     " ) or tr.passed is null"
++ " group by m.id,emp.id"
++ " order by emp.id, m.id";
         try {
-            Criteria crit = this.getSession().createCriteria(Employees.class,"emp");
-            crit.createCriteria("Measures", "m");
-            crit.createAlias("testResultses", "tr",Criteria.LEFT_JOIN);
-            crit.createAlias("tests", "t",Criteria.LEFT_JOIN
-                                ,Restrictions.eqProperty("t.measures.id", "m.id"));
-            crit.addOrder(Order.asc("emp.id").asc("m.id"));
-            // crit.add(Restrictions.between("id.passed", startDate, endDate));
-            res = crit.list();
+            Query sqlQuery = this.getSession().createSQLQuery(query);
+            sqlQuery.setString("startDate", df.format(startDate));
+            sqlQuery.setString("endDate", df.format(endDate));
+            sqlQuery.executeUpdate();
         } catch (RuntimeException re) {
             throw re;
         }
-        return res;
     }
 }
